0ad325f5a9250c90d34b701802403c35eb63709b
josephineremy <jremy2@audencia.com>
2020-11-02 16:05:35 +0100

    First commit and pushing to Heroku

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
+15.times do |x|
+  f_name=Faker::Name.first_name
+  User.create(description: Faker::Quote.famous_last_words, first_name: f_name, last_name: Faker::Name.last_name, email: f_name + "@yopmail.com", encrypted_password: 'azerty')
+    puts "L'utilisateur #{f_name} a bien été créé"
+end
+
+25.times do |x|
+    Event.create(start_date: rand(Time.parse("2020-11-02 20:00:00")..Time.parse("2022-01-01 00:00:00")), duration: rand(5..100)*5, description: Faker::Lorem.paragraph_by_chars(number: 300, supplemental: false), location: Faker::Address.city, price: rand(1..1000), title: Faker::Book.title, admin: User.all.sample)
+  puts "L'évenement nb #{x + 1} a été créé"
+end
+
+20.times do |x|
+  Attendance.create(user: User.all.sample, event: Event.all.sample)
+  puts "Le participant nb #{x + 1} a été créé"
+end
