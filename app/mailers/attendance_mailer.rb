f383094a665175db734a16e204e1e44f3567ef31
josephineremy <jremy2@audencia.com>
2020-11-02 17:42:00 +0100

    send mails participants

---
+++ app/mailers/attendance_mailer.rb
@@ -0,0 +1,10 @@
+class AttendanceMailer < ApplicationMailer
+  default from: 'jremy2@audencia.com'
+
+  def inscription_event(attendance)
+  @attendance = attendance
+  @admin = attendance.event.admin
+  @url = 'https://event-brite-light.herokuapp.com/'
+  mail(to: @attendance.event.admin.email, subject: 'Une nouvelle inscription à votre event !')
+  end
+end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,9 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  after_create :inscription_event_send
+  def inscription_event_send
+		AttendanceMailer.inscription_event(self).deliver_now
+  end
 end
